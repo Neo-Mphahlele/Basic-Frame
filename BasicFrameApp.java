4b928b9bec3484f60df0d82f73ef1a5e84ff4d49
Neo-Mphahlele <221333047@tut4life.ac.za>
2024-09-26 13:48:47 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package basicframeapp;
+import za.ac.tut.ui.MyFirstFrame;
+/**
+ *
+ * @author Acer
+ */
+public class BasicFrameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // declare frame as variable
+        MyFirstFrame myFrame;
+        
+        //instantiate the frame
+        myFrame = new MyFirstFrame();
+    }
+    
+}
